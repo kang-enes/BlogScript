a0d4f874b042d41701862a7a773db443a8aca04c
Nandang Solehudin <peace.enes@gmail.com>
2025-01-28 15:40:48 +0700

    scrolltopdown

---
+++ scrolltopdown.js
@@ -0,0 +1,15 @@
+//<![CDATA[
+jQuery.noConflict();
+jQuery(function(){ 
+
+    jQuery('a[href*=#]').click(function() {   
+    if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') 
+        && location.hostname == this.hostname) {        
+            var $target = jQuery(this.hash);            
+            $target = $target.length && $target || jQuery('[name=' + this.hash.slice(1) +']');            
+            if ($target.length) {            
+                var targetOffset = $target.offset().top;                
+                jQuery('html,body').animate({scrollTop: targetOffset}, 1000);                    
+                return false; } }        
+    }); });
+//]]>
