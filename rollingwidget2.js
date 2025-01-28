5870b153a604c7775964f321d85080eb5e5fabf1
Nandang Solehudin <peace.enes@gmail.com>
2025-01-28 08:53:23 +0700

    rollingwidget2

---
+++ rollingwidget2.js
@@ -0,0 +1,4 @@
+var gb = document.getElementById("gb");
+gb.style.right = (30-gb.offsetWidth).toString() + "px";
+document.write('<a href="javascript:showHideGB()"><img style="float:left; border:0px;" src="https://lh4.googleusercontent.com/-aDhfI0RQAmA/UCqHWZBcpHI/AAAAAAAAGO4/7rbIexUs8N8/close.gif" title="Close/Tutup" /></a>');
+document.write('<div style="font-family:Arial; font-size:8pt; float:left; padding-left:5px; margin-top:15px;"><a href="http://ruangsc.blogspot.com/2010/02/menyembunyikan-rolling-widget-buku-tamu.html" target="_blank" title="Cara menyembunyikan Rolling Widget">Widget by SC Community</a></div>');
