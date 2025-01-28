ee76b453529c824371c7023a55447572c3c05046
Nandang Solehudin <peace.enes@gmail.com>
2025-01-28 08:56:29 +0700

    sc-popularpost

---
+++ sc-popularpost.js
@@ -0,0 +1,9 @@
+//<![CDATA[
+var postTitlear=new Array();var postUrlar=new Array();var postCommentar=new Array();var totalpost;document.write("<ul>");function showrecentposts(j){var a=j.feed.entry.length;totalpost=a;for(var f=0;f<a;f++){var h=j.feed.entry[f];var c=h.title.$t;var b;var g;if(f==j.feed.entry.length){break}
+for(var d=0;d<h.link.length;d++){if(h.link[d].rel=="alternate"){g=h.link[d].href;break}}
+for(var d=0;d<h.link.length;d++){if(h.link[d].rel=="replies"&&h.link[d].type=="text/html"){b=h.link[d].title.split(" ")[0];break}}c=(titlebold)?"<b>"+c+"</b>":c;postTitlear.push(c);postUrlar.push(g);postCommentar.push(b)}sortPosts();for(var f=0;f<maxshowresult;f++){var e='<li><a href="'+postUrlar[f]+'" class="recent-link">'+postTitlear[f]+" {"+postCommentar[f]+"  "+explanation+"}</a>";document.write(e)}}
+function sortPosts(){function c(d,f){var e=postTitlear[d];postTitlear[d]=postTitlear[f];postTitlear[f]=e;var e=postUrlar[d];postUrlar[d]=postUrlar[f];postUrlar[f]=e;var e=postCommentar[d];postCommentar[d]=postCommentar[f];postCommentar[f]=e}
+for(var b=0;b<postTitlear.length-1;b++){for(var a=b+1;a<postTitlear.length;a++){if(parseInt(postCommentar[b])<parseInt(postCommentar[a])){c(b,a)}}}}
+document.write('<script src="https://'+home_page+"/feeds/posts/default?max-results="+numposts+'&orderby=published&alt=json-in-script&callback=showrecentposts"><\/script>');document.write('</li>')
+document.write('<div style="font-size:11px; text-shadow:2px 2px 2px #adadad; margin-top:5px;"><a href="https://ruangsc.blogspot.com/2010/02/menambahkan-most-popular-post-artikel.html" target="_blank" title="Cara membuat Most Populars Post Widget">Widget by SC Community</a></div>');
+//]]>
