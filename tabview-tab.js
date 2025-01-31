b67e1b81394aa14d9a60eb08c9fbda6c77c0ab04
Nandang Solehudin <peace.enes@gmail.com>
2025-01-31 20:03:35 +0700

    tabview-tab

---
+++ tabview-tab.js
@@ -0,0 +1,4 @@
+//<![CDATA[
+tabview_initialize('TabView');
+document.write('<div style="font-family:Arial; font-size:85%; margin-top:3px; text-align:right;"><a href="http://ruangsc.blogspot.com/2009/07/cara-membuat-tab-view-widget-pada.html" target="_blank" title="Cara membuat TabView Widget">Widget by SC Community</a></div>');
+//]]>
