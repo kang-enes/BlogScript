82bfbc4f71b872c34649fd6f20c74721f880b96e
Nandang Solehudin <peace.enes@gmail.com>
2025-02-01 09:43:02 +0700

    rollingwidget

--- rollingwidget.js
+++ rollingwidget.js
@@ -1,9 +1,11 @@
-// ----------------------------------------
-// ROLLING WIDGET
-// ----------------------------------------
-//
+// --------------------------------
+// HIDEN ROLLING WIDGET
+// Author:  Kang eNeS
+// --------------------------------
 
+//<![CDATA[
 var gb = document.getElementById("gb");
 gb.style.right = (30-gb.offsetWidth).toString() + "px";
 document.write('<a href="javascript:showHideGB()"><img style="float:left; border:0px;" src="https://images2.imgbox.com/a6/6c/hUUzGYx8_o.gif" title="Close/Tutup" /></a>');
 document.write('<div style="font-family:Arial; font-size:8pt; float:left; padding-left:5px; margin-top:15px;"><a href="http://ruangsc.blogspot.com/2009/08/membuat-rolling-widget-buku-tamu-widget.html" target="_blank" title="Cara membuat Rolling Widget">Widget by SC Community</a></div>');
+//]]>
