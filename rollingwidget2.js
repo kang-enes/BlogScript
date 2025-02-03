c326bb2360beedbc18eed0f01889235e24fd9ba8
Nandang Solehudin <peace.enes@gmail.com>
2025-02-03 22:15:42 +0700

    rollingwidget2

--- rollingwidget2.js
+++ rollingwidget2.js
@@ -6,6 +6,6 @@
 //<![CDATA[
 var gb = document.getElementById("gb");
 gb.style.right = (30-gb.offsetWidth).toString() + "px";
-document.write('<a href="javascript:showHideGB()"><img style="float:left; border:0px;" src="https://images2.imgbox.com/a6/6c/hUUzGYx8_o.gif" title="Close/Tutup" /></a>');
+document.write('<a href="javascript:showHideGB()"><img style="float:left; border:0px;" src="https://blogger.googleusercontent.com/img/b/R29vZ2xl/AVvXsEiym5PglRVttx-RSi3SqmibPq_N3YwCTokABRJWFZgryEFSg7rUC6AAh9RS8kJ6vnieBokrCUq8ZjITj3Xc6PutG_Sd0nd4jVRt_aZcUJBL83-kwsn_PgTdRCk-Qj0Ik3h6TZq5-sMFD9g_KRJBseMJ0Tud8hyphenhyphenscoBJUpV6U0kwrr8u2z8lWSY0yUDicEfE/s1600/close.gif" title="Close/Tutup" /></a>');
 document.write('<div style="font-family:Arial; font-size:8pt; float:left; padding-left:5px; margin-top:15px;"><a href="http://ruangsc.blogspot.com/2010/02/menyembunyikan-rolling-widget-buku-tamu.html" target="_blank" title="Cara menyembunyikan Rolling Widget">Widget by SC Community</a></div>');
 //]]>
