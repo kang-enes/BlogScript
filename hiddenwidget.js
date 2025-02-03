4554f22e95ec4d8f6875ea40dfc85c4ca1309548
Nandang Solehudin <peace.enes@gmail.com>
2025-02-03 22:16:15 +0700

    hiddenwidget

--- hiddenwidget.js
+++ hiddenwidget.js
@@ -45,7 +45,7 @@ function writex(str){
     document.getElementById("wx").innerHTML += str + " ";
 }
 gb_setInitBehaviour();
-document.write('<div style="float:left;"><a href="javascript:void(0)" onclick="gb_showHideGB()"><img style="float:left; border:0px;" src="http://i563.photobucket.com/albums/ss76/peace_enes/SC/close.gif" title="Close/Tutup" /></a></div>');
+document.write('<div style="float:left;"><a href="javascript:void(0)" onclick="gb_showHideGB()"><img style="float:left; border:0px;" src="https://blogger.googleusercontent.com/img/b/R29vZ2xl/AVvXsEiym5PglRVttx-RSi3SqmibPq_N3YwCTokABRJWFZgryEFSg7rUC6AAh9RS8kJ6vnieBokrCUq8ZjITj3Xc6PutG_Sd0nd4jVRt_aZcUJBL83-kwsn_PgTdRCk-Qj0Ik3h6TZq5-sMFD9g_KRJBseMJ0Tud8hyphenhyphenscoBJUpV6U0kwrr8u2z8lWSY0yUDicEfE/s1600/close.gif" title="Close/Tutup" /></a></div>');
 document.write('<div style="font-family:Arial; font-size:8pt; float:left; padding-left:5px;  margin-top:15px;"><a href="http://ruangsc.blogspot.com/2010/03/membuat-hidden-widget-buku-tamu-widget.html" target="_blank" title="Cara membuat Hidden Widget">Widget by SC Community</a></div>');
 
 //]]>
