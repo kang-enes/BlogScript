8fa88ccbb41c50f3ca7c8f50cfb60bcec60c73c8
Nandang Solehudin <peace.enes@gmail.com>
2025-02-11 21:07:55 +0700

    alljs-nesack

--- alljs-nesack.js
+++ alljs-nesack.js
@@ -90,21 +90,6 @@ jQuery(function(){
 
 });
 
-/*--- Scrolltopdown ---*/
-jQuery.noConflict();
-jQuery(function(){ 
-	
-    jQuery('a[href*=#]').click(function() {   
-    if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') 
-        && location.hostname == this.hostname) {        
-            var $target = jQuery(this.hash);            
-            $target = $target.length && $target || jQuery('[name=' + this.hash.slice(1) +']');            
-            if ($target.length) {            
-                var targetOffset = $target.offset().top;                
-                jQuery('html,body').animate({scrollTop: targetOffset}, 1000);                    
-                return false; } }        
-    }); });
-
 /*--- 
 Script JQuery Lainnya
 Deskripsi: Toogle, stickysocial, Scrolltopdown, dll.
