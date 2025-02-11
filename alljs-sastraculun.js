796bd9b3618bb74927fd6dea9d0ad4b7b2ba92f8
Nandang Solehudin <peace.enes@gmail.com>
2025-02-11 20:25:07 +0700

    alljs-sastraculun

--- alljs-sastraculun.js
+++ alljs-sastraculun.js
@@ -1,4 +1,4 @@
-/* Script untuk Sastra Culun (http://sastraculun.blogspot.com) */
+/*--- Script untuk Sastra Culun (http://sastraculun.blogspot.com) ---*/
 
 // ----------------------------------------
 // SHOW RAINBOW EFFECT FOR LINK HOVER
@@ -188,27 +188,26 @@ Deskripsi: Untuk sagalarupa
 jQuery.noConflict();
 jQuery(document).ready(function(){ 
 
-/* toggle */
+/*--- toggle ---*/
   jQuery(".toggle-content").hide(); 
   jQuery("#toggle-container>h4").click(function(){ jQuery(this).toggleClass("active").next().slideToggle("slow"); });
 
-/* sidebars */
+/*--- widget content list ---*/
 jQuery('.widget-content li a').hover(function() {
   jQuery(this).animate({ paddingLeft: '17px' }, {duration:250});}, function() {
   jQuery(this).animate({ paddingLeft: '15px' }, {duration:250});});
 
-/* sociable img */
+/*--- sociable img ---*/
   jQuery('.sociable img').fadeTo('slow', 0.4); jQuery('.sociable img').hover(function() { jQuery(this).fadeTo('slow', 1); }, function() { jQuery(this).fadeTo('slow', 0.4); });
 
-/* sharethis_toolbox, stickysocial, luhur-handap */
-  jQuery('.sharethis_toolbox img, .stickysocial img, .luhur-handap img').fadeTo('slow', 0.75); 
-  jQuery('.sharethis_toolbox img, .stickysocial img, .luhur-handap img').hover(function() { 
+/*--- stickysocial dan luhur-handap ---*/
+  jQuery('.stickysocial img, .luhur-handap img').fadeTo('slow', 0.75); 
+  jQuery('.stickysocial img, .luhur-handap img').hover(function() { 
     jQuery(this).fadeTo('slow', 1); }, function() { jQuery(this).fadeTo('slow', 0.75); });});
 
-/* Scrolltopdown */
+/*--- Scrolltopdown ---*/
 jQuery(function(){ jQuery('a[href*=#]').click(function(){
   if(location.pathname.replace(/^\//,'')==this.pathname.replace(/^\//,'')&&location.hostname==this.hostname){
   var $target=jQuery(this.hash);$target=$target.length&&$target||jQuery('[name='+this.hash.slice(1)+']');if($target.length){
     var targetOffset=$target.offset().top;jQuery('html,body').animate({scrollTop:targetOffset},1000);return false;}}});
-
 });
