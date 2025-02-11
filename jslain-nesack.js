ed0fc54e4cd080b8f638d9a7166bc9609d161580
Nandang Solehudin <peace.enes@gmail.com>
2025-02-11 22:32:37 +0700

    jslain-nesack

---
+++ jslain-nesack.js
@@ -0,0 +1,56 @@
+/*--- Script untuk NeSaCk oleh Kang eNeS (https://ruangsc.blogspot.com) ---*/
+
+/*--- 
+Script: jQuery.js
+Deskripsi: Untuk sagalarupa
+---*/ 
+
+/*--- Scrolltopdown ---*/  
+jQuery.noConflict();
+jQuery(function(){ 
+
+    jQuery('a[href*=#]').click(function() {   
+    if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') 
+        && location.hostname == this.hostname) {        
+            var $target = jQuery(this.hash);            
+            $target = $target.length && $target || jQuery('[name=' + this.hash.slice(1) +']');            
+            if ($target.length) {            
+                var targetOffset = $target.offset().top;                
+                jQuery('html,body').animate({scrollTop: targetOffset}, 1000);                    
+                return false; } }        
+    }); });
+
+/*--- toggle spoiler ---*/ 
+    jQuery(".eusi-sorolok").hide(); 
+    jQuery("#sorolok>h4").click(function(){ 
+        jQuery(this).toggleClass("active").next().slideToggle("slow"); 
+    });
+
+/*--- widget content list ---*/  
+    jQuery(document).ready(function(){ 
+        jQuery('.widget-content li a').hover(function() {
+            jQuery(this).animate({ paddingLeft: '5px' }, {duration:250});
+            }, function() {
+            jQuery(this).animate({ paddingLeft: '0px' }, {duration:250});           
+       });
+    });
+
+/*--- 
+Script: Non-jQuery.js
+Deskripsi: Untuk Togglespoiler
+---*/ 
+
+/*--- v-toggle ---*/ 
+    window.addEvent('domready', function() {
+    var myVerticalSlide = new Fx.Slide('v_juljol').hide(); $('v_toggle').addEvent('click', function(event){ event.stop(); 
+        myVerticalSlide.toggle(); }); $('v_nyumput').addEvent('click', function(event){ event.stop(); 
+        myVerticalSlide.slideOut(); });
+    });
+    
+/*--- Togglespoiler ---*/ 
+function togglespoiler (postid){
+    var whichpost = document.getElementById(postid);
+    if (whichpost.className=='widgetshown'){
+    whichpost.className='widgethidden';}
+    else{
+    whichpost.className='widgetshown';}}
