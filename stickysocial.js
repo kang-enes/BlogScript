d6d7bd200bbc2fa563d9feecbe94fd2b519b38b1
Nandang Solehudin <peace.enes@gmail.com>
2025-02-11 18:23:26 +0700

    stickysocial

--- stickysocial.js
+++ stickysocial.js
@@ -1,23 +1,19 @@
-// ----------------
-// STICKYSOCIAL
-// ----------------
-      jQuery.noConflict();
-      jQuery(document).ready(function(){ 
-        jQuery(&quot;.toggle-content&quot;).hide();  
-        jQuery(&quot;#toggle-container&gt;h4&quot;).click(function(){
-               jQuery(this).toggleClass(&quot;active&quot;).next().slideToggle(&quot;slow&quot;);
-      });
-      /* sidebars */
-      jQuery(&#39;.widget-content li a&#39;).hover(function() {
-             jQuery(this).animate({ paddingLeft: &#39;17px&#39; }, {duration:250});
-                                  }, function() {
-                                    jQuery(this).animate({ paddingLeft: &#39;15px&#39; }, {duration:250});           
-                                                         });
-                                    /* stickysocial */
-                                    jQuery(&#39;.stickysocial img&#39;).fadeTo(&#39;slow&#39;, 0.88);
-                                           jQuery(&#39;.stickysocial img&#39;).hover(function() {
-                                           jQuery(this).fadeTo(&#39;slow&#39;, 1);
-                                                               }, function() {
-                                      jQuery(this).fadeTo(&#39;slow&#39;, 0.88);
-                                                          });
-                                    });
+/* Sagala Rupa */
+
+jQuery.noConflict();
+jQuery(document).ready(function(){ 
+
+/* toggle */
+  jQuery(".toggle-content").hide(); 
+  jQuery("#toggle-container>h4").click(function(){ jQuery(this).toggleClass("active").next().slideToggle("slow"); });
+
+/* sidebars */
+jQuery('.widget-content li a').hover(function() {
+  jQuery(this).animate({ paddingLeft: '17px' }, {duration:250});}, function() {
+  jQuery(this).animate({ paddingLeft: '15px' }, {duration:250});}); });
+  
+ /* sharethis_toolbox, stickysocial, luhur-handap */
+ jQuery('.sharethis_toolbox img, .stickysocial img, .luhur-handap img').fadeTo('slow', 0.75); 
+ jQuery('.sharethis_toolbox img, .stickysocial img, .luhur-handap img').hover(function() { 
+   jQuery(this).fadeTo('slow', 1); }, function() { jQuery(this).fadeTo('slow', 0.75); });});
+
